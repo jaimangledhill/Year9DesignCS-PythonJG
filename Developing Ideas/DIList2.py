e32d05fe9a6830ac905c43a2418c94e880aaca83
Jaiman.Gledhill <jaiman.gledhill@Gledhill.local>
2018-11-10 21:47:39 -0500

    Adding Drop Down Menu

---
+++ Developing Ideas/DIList2.py
@@ -0,0 +1,11 @@
+food1 = input("Input food 1: ")
+food2 = input("Input food 2: ")
+food3 = input("Input food 3: ")
+
+
+#Process
+foods = [food1, food2, food3] #Data structure
+
+#output -we want to sort our lists in our programs
+foods.sort()
+print (foods)
